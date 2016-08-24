32310dd5f1d4dd82f3364af50843d6229fb933b5
UpperApps <upperapps@gmail.com>
2016-08-24 20:48:06 -0300

    Conclusão da aula 3

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-var clean = require('gulp-clean');
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin');
+
+//Obs.:
+//1- O Gulp por padrão trabalha assincronamente, ao contrário do Grunt que é
+//síncrono.
+//O Gulp trabalha com os dados em memória e faz apenas uma leitura e uma gravação
+//em disco, diferentemente do Grunt que para cada transação lê e grava os dados
+//em disco. Isso torna o Gulp ainda mais performático.
+
+// tarefa padrão que chama todas as outras. Seria o método main do script.
+gulp.task('default', ['copy'], function() {
+  gulp.start('build-img', 'usemin');
+});
 
 //Limpa a pasta de distribuição para receber os novos arquivos.
 gulp.task('clean', function() {
@@ -15,8 +32,17 @@ gulp.task('copy', ['clean'], function() {
 });
 
 //A minificação das imagens na pasta de distribuição (dist).
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+  return gulp.src('dist/**/*.html')
+  .pipe(usemin({
+    js: [uglify],
+    css: [cssmin]
+  }))
+  .pipe(gulp.dest('dist'))
 });
